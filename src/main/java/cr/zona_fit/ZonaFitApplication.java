9a5b81ac09ca9aeb1c1e7ac1cfb2d30af6002ab7
carlosninacio <cigramirez@poligran.edu.co>
2026-01-08 15:03:25 -0500

    Configuración clase para la presentación en consola

--- src/main/java/cr/zona_fit/ZonaFitApplication.java
+++ src/main/java/cr/zona_fit/ZonaFitApplication.java
@@ -1,13 +1,30 @@
 package cr.zona_fit;
 
+import cr.zona_fit.servicio.IClienteServicio;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZonaFitApplication {
+public class ZonaFitApplication implements CommandLineRunner {
+
+    @Autowired
+    private IClienteServicio clienteServicio;
+
+    private static final Logger logger = LoggerFactory.getLogger(ZonaFitApplication.class);
 
 	public static void main(String[] args) {
+        logger.info("Iniciando la aplicación");
+        // Levantar fabrica de Spring
 		SpringApplication.run(ZonaFitApplication.class, args);
+        logger.info("Aplicación finalizada!");
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("*** Aplicación Zona Fit Gym ***");
+    }
 }
