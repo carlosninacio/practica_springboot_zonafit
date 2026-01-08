97a3b7a234c997c8e538173525ecedb366f19006
carlosninacio <cigramirez@poligran.edu.co>
2026-01-08 14:42:42 -0500

    Creación de la clase servicio

---
+++ src/main/java/cr/zona_fit/servicio/ClienteServicio.java
@@ -0,0 +1,37 @@
+package cr.zona_fit.servicio;
+
+import cr.zona_fit.modelo.Cliente;
+import cr.zona_fit.repositorio.ClienteRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteServicio implements IClienteServicio {
+
+    @Autowired
+    private ClienteRepositorio clienteRepositorio;
+
+    @Override
+    public List<Cliente> listarClientes() {
+        List<Cliente> clientes = clienteRepositorio.findAll();
+        return clientes;
+    }
+
+    @Override
+    public Cliente buscarClientePorId(Integer idCliente) {
+        Cliente cliente = clienteRepositorio.findById(idCliente).orElse(null); // Or Else null por si no se encuentra
+        return cliente;
+    }
+
+    @Override
+    public void guardarCliente(Cliente cliente) {
+        clienteRepositorio.save(cliente);
+    }
+
+    @Override
+    public void eliminarCliente(Cliente cliente) {
+        clienteRepositorio.delete(cliente);
+    }
+}
--- src/main/java/cr/zona_fit/servicio/IClienteServicio.java
+++ src/main/java/cr/zona_fit/servicio/IClienteServicio.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface IClienteServicio {
     public List<Cliente> listarClientes();
-    public Cliente buscarClientePorId(Cliente cliente);
+    public Cliente buscarClientePorId(Integer idCliente);
     public void guardarCliente(Cliente cliente);
     public void eliminarCliente(Cliente cliente);
 }
