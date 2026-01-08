e91f46983ff2090f97ff4783cc8febbd7e750204
carlosninacio <cigramirez@poligran.edu.co>
2026-01-08 14:31:12 -0500

    Creación de la interfaz del servicio

--- src/main/java/cr/zona_fit/repositorio/ClienteRepositorio.java
+++ src/main/java/cr/zona_fit/repositorio/ClienteRepositorio.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 // Tipo de la clase y el tipo de la llave primaria
 public interface ClienteRepositorio extends JpaRepository<Cliente, Integer> {
-
 }
---
+++ src/main/java/cr/zona_fit/servicio/IClienteServicio.java
@@ -0,0 +1,10 @@
+package cr.zona_fit.servicio;
+import cr.zona_fit.modelo.Cliente;
+import java.util.List;
+
+public interface IClienteServicio {
+    public List<Cliente> listarClientes();
+    public Cliente buscarClientePorId(Cliente cliente);
+    public void guardarCliente(Cliente cliente);
+    public void eliminarCliente(Cliente cliente);
+}
