67e1f6660f284147b4ae2d51d1011010937f9316
carlosninacio <cigramirez@poligran.edu.co>
2026-01-08 14:07:49 -0500

    Creación clase entidad

---
+++ src/main/java/cr/zona_fit/modelo/Cliente.java
@@ -0,0 +1,22 @@
+package cr.zona_fit.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data // Genera metodos de get y set a esta clase
+@NoArgsConstructor // Constructor vacio
+@AllArgsConstructor // Constructor con todos los argumentos
+@ToString
+@EqualsAndHashCode
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private Integer membresia;
+}
