02e41e7b57a370442340e722dcf149fa949622a2
carlosninacio <cigramirez@poligran.edu.co>
2026-01-08 17:35:50 -0500

    Menú para la presentación

--- src/main/java/cr/zona_fit/ZonaFitApplication.java
+++ src/main/java/cr/zona_fit/ZonaFitApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class ZonaFitApplication implements CommandLineRunner {
 
@@ -16,15 +18,39 @@ public class ZonaFitApplication implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(ZonaFitApplication.class);
 
+    String nl = System.lineSeparator(); // Caracter de salto de linea generico
+
 	public static void main(String[] args) {
         logger.info("Iniciando la aplicación");
         // Levantar fabrica de Spring
 		SpringApplication.run(ZonaFitApplication.class, args);
-        logger.info("Aplicación finalizada!");
 	}
 
     @Override
     public void run(String... args) throws Exception {
-        logger.info("*** Aplicación Zona Fit Gym ***");
+        zonaFitApp();
+    }
+
+    private void zonaFitApp() {
+        var salir = false;
+        var consola = new Scanner(System.in);
+        while (!salir) {
+            var opcion = mostrarMenu(consola);
+            // salir = ejecutarOpciones(consola, opcion);
+            logger.info(nl);
+        }
+    }
+
+    private int mostrarMenu(Scanner consola) {
+        logger.info("""
+        \n*** Aplicación Zona Fit Gym ***
+        \t1. Listar Clientes
+        \t2. Buscar Cliente
+        \t3. Agregar Cliente
+        \t4. Modificar Cliente
+        \t5. Eliminar Cliente
+        \t6. Salir
+        * Elige una opción:\s""");
+        return Integer.parseInt(consola.nextLine());
     }
 }
